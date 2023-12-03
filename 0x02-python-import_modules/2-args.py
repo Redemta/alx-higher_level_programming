b75e63f3e03d8c54746d6996d745ea0e498b584e
Redemta <redzawy@gmail.com>
2023-12-03 00:26:35 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    num_arguments = len(sys.argv) - 1
+    if num_arguments == 0:
+        print("0 arguments.")
+    elif num_arguments == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_arguments))
+    for i in range(num_arguments):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
